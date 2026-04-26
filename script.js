ee69cd1b2629339058ab0ef4031af1bc75c637aa
shnekars <shnekars@gmail.com>
2026-04-26 15:59:11 +0300

    У файлі script.js добавлено обробку кліку на кнопках видалення непотрібних завдань

--- script.js
+++ script.js
@@ -40,3 +40,51 @@ input.addEventListener('keypress', (e) => {
         addTask();
     }
 });
+
+// Функція для додавання до списку завдань нового завдання
+// з добавленням кнопки видалення
+function addTask() {
+    const taskText = taskInput.value.trim();
+ 
+    if (taskText === "") {
+        alert("Будь ласка, введіть текст завдання!");
+        return;
+    }
+ 
+    // Створюємо елемент завдання (контейнер label)
+    const label = document.createElement('label');
+    label.className = 'task-list-item';
+ 
+    // Наповнюємо його структурними елементами
+    label.innerHTML = `
+        <input type="checkbox">
+        <span class="task-checkmark"></span>
+        <span class="task-text">${taskText}</span>
+        <button class="task-delete-btn" title="Видалити завдання">✖</button>
+    `;
+ 
+    // Додаємо подію для кнопки видалення завдання
+    const taskDeleteBtn = label.querySelector('.task-delete-btn');
+    taskDeleteBtn.addEventListener('click', () => {
+        label.remove(); // Видаляємо завдання
+    });
+ 
+    // Додаємо нове завдання в список
+    taskList.appendChild(label);
+ 
+    // Очищаємо поле вводу
+    taskInput.value = "";
+    taskInput.focus();
+}
+ 
+ // Функція для обробки подій на статичних елементах списку
+function attachTaskEvents(label) {
+    // Додаємо подію для кнопки видалення завдання
+    const taskDeleteBtn = label.querySelector('.task-delete-btn');
+    taskDeleteBtn.addEventListener('click', () => {
+        label.remove(); // Видаляємо завдання
+    });
+}
+ 
+// Навішуємо функцію обробки подій на статичні елементи списку
+document.querySelectorAll('#task-list label').forEach(attachTaskEvents);
